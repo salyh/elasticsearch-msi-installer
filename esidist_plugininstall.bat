dc781d7aca689cc8c3f24d28bee2cc0a1ef77912
salyh <hendrikdev22@gmail.com>
2014-01-10 13:19:44 +0100

    update to ea 0.9.10

--- esidist_plugininstall.bat
+++ esidist_plugininstall.bat
@@ -1,5 +1,5 @@
 @echo off
 rem Configure the plugins you want to install
-call esi_plugin.bat -i  elasticsearch/elasticsearch-analysis-phonetic/1.7.0
+call esi_plugin.bat -i  elasticsearch/elasticsearch-analysis-phonetic/1.8.0
 call esi_plugin.bat -i  elasticsearch/elasticsearch-mapper-attachments/1.9.0
 rem call esi_plugin.bat -url http://bit.ly/19RBF3b -install langdetect
