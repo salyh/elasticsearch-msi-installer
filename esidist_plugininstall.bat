f49f88ddd05444cb03d4f06eff6aa7d9f44f94e2
salyh <hendrikdev22@gmail.com>
2015-04-01 19:22:10 +0200

    update plugins for es 1.5.0

--- esidist_plugininstall.bat
+++ esidist_plugininstall.bat
@@ -3,6 +3,6 @@ rem Copyright 2014-2015 Hendrik Saly
 rem Licensed under the Apache License, Version 2.0
 
 rem Configure the plugins you want to install
-call esi_plugin.bat -i  elasticsearch/elasticsearch-analysis-phonetic/2.3.0
-call esi_plugin.bat -i  elasticsearch/elasticsearch-mapper-attachments/2.3.0
+call esi_plugin.bat -i  elasticsearch/elasticsearch-analysis-phonetic/2.5.0
+call esi_plugin.bat -i  elasticsearch/elasticsearch-mapper-attachments/2.5.0
 rem call esi_plugin.bat -url http://bit.ly/19RBF3b -install langdetect
